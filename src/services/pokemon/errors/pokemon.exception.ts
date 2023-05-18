17f73710ab20200d8ac71bcf3b7484074476f1e2
Jonathan Ricardo <mirante-jonathan.reichel@ailos.coop.br>
2023-05-18 17:22:06 -0300

    🔥 removed feature

--- src/services/pokemon/errors/pokemon.exception.ts
+++
@@ -1,8 +0,0 @@
-import { Exception } from "../../../core/errors/exception";
-
-export class PokmemonException extends Exception {
-  constructor(message: string) {
-    super(message);
-    this.name = "PokemonException";
-  }
-}
--- src/services/pokemon/errors/pokemon.failure.ts
+++
@@ -1,7 +0,0 @@
-import { Failure } from "../../../core/errors/failure";
-
-export class PokemonFailure extends Failure {
-  constructor(message: string) {
-    super(message);
-  }
-}
